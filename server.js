f401f3e255e8cb16e441604afff370fcd5503e11
alejandrohn <alehn96@gmail.com>
2018-03-09 11:07:57 -0500

    add mongodb to project

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const entities = require('seneca-entity');
 const seneca = require('seneca')();
 const senecaServer = seneca
 	.use(entities)
-	.use('jsonfile-store', {
-		folder: './data'
-	})
 	.use('seneca-amqp-transport')
 	.use('codegen', { seneca: seneca })
+	.use('mongo-store', {
+		name: 'codegendb',
+		host: '127.0.0.1',
+		port: 27017,
+		options: {}
+	})
 	.listen(config.conn);
